32a687421e475e52eb58ff36d8b410ea75e32c28
poojarijayaprakash4 <pjayaprakash072@gmail.com>
2026-08-05 14:43:42 +0530

    dotEnv configuration is done✅

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("hello world")
+const express = require('express') 
+const dotenv = require('dotenv')
+
+
+const app = express();
+
+dotenv.config()
+const port = process.env.PORT
+
+app.listen(port , ()=>{
+    console.log(`Server is running at port ${port}`);
+})
+
+app.get('/home', (req,res)=>{
+    res.send("welcome to user")
+})
+app.get('/', (req,res)=>{
+    res.send("<h1> welcome to Foodtech </h1>")
+})
