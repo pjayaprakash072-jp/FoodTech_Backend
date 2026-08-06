d839ef7984a7370101c83d1b8b65138c8a47cfeb
poojarijayaprakash4 <pjayaprakash072@gmail.com>
2026-08-06 16:53:03 +0530

    Image routes added

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -4,11 +4,19 @@ const router = express.Router();
 
 
 const productController = require('../controllers/productController');
-const { route } = require('./vendorRoutes');
 
+const path = require('path')
 
 router.post('/add-product/:rgid',productController.addProduct)
 
 router.get('/:rgid/products',productController.getProductByRGroup)
+
+router.get('/uploads/:imageName',(req,res)=>{
+    const imageName = req.params.imageName;
+
+    res.headersSent('Content-Type','image/jpeg');
+
+    res.sendFile(Path.join(__dirname,'..','uploads',imageName));
+})
 
 module.exports = router
--- routes/rgroupRoutes.js
+++ routes/rgroupRoutes.js
@@ -1,11 +1,20 @@
 const express = require('express')
 const RGroupController = require('../controllers/RGroupController')
 const verifyToken = require('../middlewares/verifyToken')
+const path = require('path')
 
 const router = express.Router();
 
 // Route to add a new Restaurant Group (only authenticated vendors)
 router.post('/add-rgroup', verifyToken, RGroupController.addRGroup);
+
+router.get('/uploads/:imageName',(req,res)=>{
+    const imageName = req.params.imageName;
+
+    res.headersSent('Content-Type','image/jpeg');
+
+    res.sendFile(Path.join(__dirname,'..','uploads',imageName));
+})
 
 // Export the router
 module.exports = router;
